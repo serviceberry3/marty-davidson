f1146cda5fca073fa2992963093a636f52bc1427
Noah Weiner <noah.weiner@yale.edu>
2020-11-26 19:23:26 -0500

    add Xcode proj, create blank view

---
+++ MartyDavidson/MartyDavidson/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MartyDavidson
+//
+//  Created by Noah Weiner on 26/11/2020.
+//  Copyright © 2020 Noah Weiner. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MartyDavidson/MartyDavidson/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MartyDavidson
+//
+//  Created by Noah Weiner on 26/11/2020.
+//  Copyright © 2020 Noah Weiner. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
