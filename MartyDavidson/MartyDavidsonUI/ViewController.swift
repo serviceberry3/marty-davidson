e44c6fd8f5b34f21487e074246f5841e1500ccf7
Noah Weiner <noah.weiner@yale.edu>
2020-11-26 21:48:07 -0500

    finish up UI

--- MartyDavidson/MartyDavidsonUI/ViewController.swift
+++ MartyDavidson/MartyDavidsonUI/ViewController.swift
@@ -19,8 +19,18 @@ class ViewController: NSViewController { //extending NSViewController
     
     
     @IBOutlet weak var martyStatusLabel: NSTextField! //'!' = guaranteed to exist from now on
-    @IBOutlet weak var enableDisableButton: NSButtonCell!
+    @IBOutlet weak var enableDisableUiButton: NSButtonCell!
+    @IBOutlet weak var enableDisableButton: NSButton!
+    @IBOutlet weak var statusImage: NSImageView!
     
+    
+    deinit {
+        if #available(OSX 10.12.2, *) {
+            self.view.window?.unbind(NSBindingName(rawValue: #keyPath(touchBar)))
+        }
+        
+        UserDefaults.standard.removeObserver(self, forKeyPath: MartyConstants.martyIsDisabled)
+    }
     
     @IBAction func enableDisableAction(_ sender: Any) {
         /*
@@ -32,6 +42,55 @@ class ViewController: NSViewController { //extending NSViewController
         
         let greeting = "Hello \(name)!"
         helloLabel.stringValue = greeting*/
+        
+        //Marty's currently disabled
+        if (self.defaults.bool(forKey: MartyConstants.martyIsDisabled)) {
+            self.defaults.set(false, forKey: MartyConstants.martyIsDisabled)
+        }
+            
+        //Marty's currently enabled
+        else {
+            self.defaults.set(true, forKey: MartyConstants.martyIsDisabled)
+        }
+        
+        //updateButtons()
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        
+        
+        //basically assert that keyPath is non-null, else return
+        guard let keyPathReceived = keyPath else {
+            return
+        }
+        
+
+        //if we care about this KVO key, update the buttons
+        if observeKeys.contains(keyPathReceived) {
+            updateButtons()
+        }
+    }
+    
+    func updateButtons() {
+        DispatchQueue.main.async {
+        
+            //if Marty is currently disabled
+            if (self.defaults.bool(forKey: MartyConstants.martyIsDisabled)) {
+                self.enableDisableButton.title = "Enable"
+                self.enableDisableUiButton.title = "Enable Marty"
+                self.martyStatusLabel.stringValue = "Marty is currently disabled"
+                self.statusImage.image = NSImage(named: NSImage.statusUnavailableName)
+            }
+                
+            //if Marty IS currently enabled
+            else {
+                self.enableDisableButton.title = "Disable"
+                self.enableDisableUiButton.title = "Disable Marty"
+                self.martyStatusLabel.stringValue = "Marty is currently enabled"
+                self.statusImage.image = NSImage(named: NSImage.statusAvailableName)
+                }
+            
+        }
     }
     
     
@@ -43,8 +102,11 @@ class ViewController: NSViewController { //extending NSViewController
         defaults = UserDefaults.standard
         
         observeKeys.forEach {
+            //add key-value observer for the key "MartyIsDisabled"
             path in defaults.addObserver(self, forKeyPath: path, options: .new, context: nil)
         }
+        
+        updateButtons()
         
     }
     
@@ -57,8 +119,5 @@ class ViewController: NSViewController { //extending NSViewController
         // Update the view, if already loaded.
         }
     }
-
-    
-
 }
 
